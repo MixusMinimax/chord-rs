e0eb14f76cf82810cb29945b5f48ac261baa1db3
Maxi Barmetler <maxi.barmetler@gmail.com>
2024-05-31 20:18:47 +0200

    generate completions

--- chord/build.rs
+++ chord/build.rs
@@ -8,11 +8,15 @@
 
 use std::path::{Path, PathBuf};
 
+use clap_complete::Shell;
+
+use crate::completions::generate_completions;
 use crate::proto::compile_proto;
 
 fn main() {
     let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
     compile_proto(&out_dir);
+    generate_completions(&out_dir, &[Shell::Bash, Shell::Zsh]);
 }
 
 mod proto {
@@ -24,6 +28,8 @@ mod proto {
     use walkdir::WalkDir;
 
     use crate::{build_rs, read_hash, write_hash};
+
+    static mut SHOULD_COMPILE: Option<bool> = None;
 
     pub fn compile_proto(out_dir: &Path) {
         let (proto_root, proto_files) = proto_files();
@@ -58,6 +64,9 @@ mod proto {
     }
 
     fn should_compile(out_dir: &Path, proto_files: &[impl AsRef<Path>]) -> bool {
+        if let Some(should_compile) = unsafe { SHOULD_COMPILE } {
+            return should_compile;
+        }
         let mut hasher = DefaultHasher::new();
         proto_files.iter().for_each(|e| match std::fs::read(e) {
             Ok(content) => content.hash(&mut hasher),
@@ -70,7 +79,7 @@ mod proto {
         if has_changed {
             write_hash(&path, new_hash);
         }
-        if build_rs::has_changed(out_dir) {
+        let should_compile = if build_rs::has_changed(out_dir) {
             warning!("build.rs has changed, recompiling proto files");
             true
         } else if has_changed {
@@ -78,11 +87,75 @@ mod proto {
             true
         } else {
             false
-        }
+        };
+        unsafe { SHOULD_COMPILE = Some(should_compile) };
+        should_compile
     }
 
     fn hash_path(out_dir: &Path) -> PathBuf {
         out_dir.join("proto.hash")
+    }
+}
+
+mod completions {
+    use std::hash::{DefaultHasher, Hash, Hasher};
+    use std::path::{Path, PathBuf};
+
+    use cargo_emit::{rerun_if_changed, warning};
+    use clap::CommandFactory;
+    use clap_complete::{generate_to, Shell};
+
+    use crate::{build_rs, read_hash, write_hash};
+    use crate::completions::args::Args;
+
+    static mut SHOULD_GENERATE: Option<bool> = None;
+
+    mod args {
+        include!("./src/args.rs");
+    }
+
+    pub fn generate_completions(out_dir: &Path, shells: &[Shell]) {
+        if !should_generate(out_dir) {
+            eprintln!("args.rs has not changed, skipping generation");
+            return;
+        }
+        rerun_if_changed!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/args.rs"));
+        let mut command = Args::command();
+        shells.iter().for_each(|shell| {
+            generate_to(*shell, &mut command, "chord", out_dir).unwrap();
+        });
+    }
+
+    fn should_generate(out_dir: &Path) -> bool {
+        if let Some(should_generate) = unsafe { SHOULD_GENERATE } {
+            return should_generate;
+        }
+        let mut hasher = DefaultHasher::new();
+        std::fs::read(concat!(env!("CARGO_MANIFEST_DIR"), "/src/args.rs"))
+            .unwrap()
+            .hash(&mut hasher);
+        let new_hash = hasher.finish();
+        let path = hash_path(out_dir);
+        let old_hash = read_hash(&path);
+        let has_changed = old_hash.map_or(true, |old_hash| old_hash != new_hash);
+        if has_changed {
+            write_hash(&path, new_hash);
+        }
+        let should_compile = if build_rs::has_changed(out_dir) {
+            warning!("build.rs has changed, generating completions");
+            true
+        } else if has_changed {
+            warning!("args.rs has changed, generating completions");
+            true
+        } else {
+            false
+        };
+        unsafe { SHOULD_GENERATE = Some(should_compile) };
+        should_compile
+    }
+
+    fn hash_path(out_dir: &Path) -> PathBuf {
+        out_dir.join("completions.hash")
     }
 }
 
